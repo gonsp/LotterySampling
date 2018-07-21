59789fd0170d1a3577238c07e043dface7c78fb5
Gonzalo Solera <gonzalo1796@gmail.com>
2018-07-21 18:17:17 +0200

    Implementing main + defining structure of the project

--- src/algorithms/Algorithm.h
+++ src/algorithms/Algorithm.h
@@ -1,7 +1,16 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
+#include <ostream>
+
 class Algorithm {
+
+public:
+    void frequent_query(std::ostream& stream);
+
+    void k_top_query(std::ostream& stream);
+
+    void process_element(std::string element);
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
-#include <iostream>
 #include "InputParser.h"
 #include "algorithms/Algorithm.h"
 #include "algorithms/lottery_sampling/LotterySampling.h"
 #include "algorithms/space_saving/SpaceSaving.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int num_args, char* args[]) {
 
-    Algorithm* algorithm;
-
     InputParser params(num_args, args);
+
+    Algorithm* algorithm;
     if(!params.has_parameter("-a") || params.get_parameter("-a") == "lottery_sampling") {
         algorithm = new LotterySampling(params);
     } else if(params.get_parameter("-a") == "space_saving") {
         algorithm = new SpaceSaving(params);
+    }
+
+    string s;
+    while(cin >> s) {
+        if(s == "q") { // It's a query over the sampled elements
+            cin >> s;
+            if(s == "f") { // "More frequent than f" query
+                float f;
+                cin >> f;
+                algorithm->frequent_query(cout);
+            } else if(s == "k") { // k-top frequent elements query
+                int k;
+                cin >> k;
+                algorithm->k_top_query(cout);
+            }
+        } else { // It's a new element in the data stream
+            algorithm->process_element(s);
+        }
     }
 
     return 0;
