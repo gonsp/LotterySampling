0c8678f420d4514f4d504381b2a0186142328522
Gonzalo Solera <gonzalo1796@gmail.com>
2018-07-21 17:45:40 +0200

    Starting implementing main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+#include "InputParser.h"
+#include "algorithms/Algorithm.h"
+#include "algorithms/lottery_sampling/LotterySampling.h"
+#include "algorithms/space_saving/SpaceSaving.h"
 
+int main(int num_args, char* args[]) {
 
-int main() {
+    Algorithm* algorithm;
+
+    InputParser params(num_args, args);
+    if(!params.has_parameter("-a") || params.get_parameter("-a") == "lottery_sampling") {
+        algorithm = new LotterySampling(params);
+    } else if(params.get_parameter("-a") == "space_saving") {
+        algorithm = new SpaceSaving(params);
+    }
+
     return 0;
 }
 
