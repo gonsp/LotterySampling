2bf9585d7b07ee35982ac719af86e3132cefd41d
Gonzalo Solera <gonzalo1796@gmail.com>
2018-08-08 12:06:07 +0200

    Adding accuracy metrics: recall and precision

---
+++ test/metrics.py
@@ -0,0 +1,26 @@
+import streams
+import instance
+
+def get_recall(instance, stream, query_name, parameter):
+    reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
+    return get_common_elements(reported_elements, real_elements) / sum(freq for _, freq in real_elements)
+    return recall
+
+
+def get_precision(instance, stream, query_name, parameter):
+    reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
+    real_freq_sum_reported_elements = sum([stream.elements[element]/stream.N for element in stream.elements.keys() & list(zip(*reported_elements))[0]])
+    return get_common_elements(reported_elements, real_elements) / real_freq_sum_reported_elements
+    return precision
+
+
+def execute_query(instance, stream, query_name, parameter):
+    query = getattr(instance, query_name)
+    reported_elements = query(parameter)
+    query = getattr(stream, query_name)
+    real_elements = query(parameter)
+    return reported_elements, real_elements
+
+
+def get_common_elements(reported_elements, real_elements):
+    return sum([real_freq for element, real_freq in real_elements if element in list(zip(*reported_elements))[0]])
