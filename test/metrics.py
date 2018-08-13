b23f8a91f89c99b086da41e7d28c226a6cf0bd90
Gonzalo Solera <gonzalo1796@gmail.com>
2018-08-13 17:19:53 +0200

    Adding new metrics + improving some of the old ones

--- test/metrics.py
+++ test/metrics.py
@@ -1,26 +1,51 @@
 import streams
 import instance
 
+def get_squared_error(instance, stream, query_name, parameter):
+    reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
+    error = 0
+    for element in reported_elements.keys() & real_elements.keys():
+        error += (reported_elements[element] - real_elements[element])**2
+    for element in reported_elements.keys() ^ real_elements.keys():
+        if element in reported_elements:
+            error += (reported_elements[element] - stream.elements[element]/stream.N)**2
+        else:
+            error += real_elements[element]**2
+    return error
+
+
+def get_recall(instance, stream, query_name, parameter):
+    reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
+    return len(reported_elements.keys() & real_elements.keys()) / len(real_elements)
+
+
+def get_precision(instance, stream, query_name, parameter):
+    reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
+    return len(reported_elements.keys() & real_elements.keys()) / len(reported_elements)
+
+
 def get_weighted_recall(instance, stream, query_name, parameter):
     reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
-    return get_common_elements(reported_elements, real_elements) / sum(freq for _, freq in real_elements)
-    return recall
+    return get_intersection_sum(reported_elements, real_elements) / sum(freq for freq in real_elements.values())
 
 
-def get__weighted_precision(instance, stream, query_name, parameter):
+def get_weighted_precision(instance, stream, query_name, parameter):
     reported_elements, real_elements = execute_query(instance, stream, query_name, parameter)
-    real_freq_sum_reported_elements = sum([stream.elements[element]/stream.N for element in stream.elements.keys() & list(zip(*reported_elements))[0]])
-    return get_common_elements(reported_elements, real_elements) / real_freq_sum_reported_elements
-    return precision
+    for element in reported_elements:
+        reported_elements[element] = 1 - reported_elements[element]
+    for element in real_elements:
+        real_elements[element] = 1 - real_elements[element]
+    real_freq_sum_reported_elements = sum([1 - stream.elements[element] / stream.N for element in reported_elements.keys()])
+    return get_intersection_sum(reported_elements, real_elements) / real_freq_sum_reported_elements
 
 
 def execute_query(instance, stream, query_name, parameter):
     query = getattr(instance, query_name)
-    reported_elements = query(parameter)
+    reported_elements = dict(query(parameter))
     query = getattr(stream, query_name)
-    real_elements = query(parameter)
+    real_elements = dict(query(parameter))
     return reported_elements, real_elements
 
 
-def get_common_elements(reported_elements, real_elements):
-    return sum([real_freq for element, real_freq in real_elements if element in list(zip(*reported_elements))[0]])
+def get_intersection_sum(reported_elements, real_elements):
+    return sum([real_elements[element] for element in reported_elements.keys() & real_elements.keys()])
