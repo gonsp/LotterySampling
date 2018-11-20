8c7a48ce3cbc4cf51adcff6256d897ff9dc8919f
Gonzalo Solera <gonzalo1796@gmail.com>
2018-11-20 23:36:24 +0100

    Adding new metric (sorting_error)

--- test/metrics.py
+++ test/metrics.py
@@ -47,11 +47,23 @@ def get_weighted_precision_2(instance, stream, query_name, parameter):
     return real_freq_sum_reported_elements / sum(freq for freq in real_elements.values())
 
 
-def execute_query(instance, stream, query_name, parameter):
+def get_sorting_error(instance, stream, query_name, k):
+    assert query_name is 'k_top_query'
+    reported_elements, _ = execute_query(instance, stream, query_name, k, original_order=True)
+    error = 0
+    for i, element in enumerate(reported_elements):
+        error += (k - i) * (1 - (stream.elements[element[0]] / stream.N))
+    return error
+
+
+def execute_query(instance, stream, query_name, parameter, original_order=False):
     query = getattr(instance, query_name)
-    reported_elements = dict(query(parameter))
+    reported_elements = query(parameter)
     query = getattr(stream, query_name)
-    real_elements = dict(query(parameter))
+    real_elements = query(parameter)
+    if not original_order:
+        reported_elements = dict(reported_elements)
+        real_elements = dict(real_elements)
     return reported_elements, real_elements
 
 
